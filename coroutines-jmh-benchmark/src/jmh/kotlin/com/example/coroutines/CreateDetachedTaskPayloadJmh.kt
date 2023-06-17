31f748bab05cee41e604a99cf60b345e04797560
Xipho <xipho@mail.ru>
2023-06-17 19:50:56 +0300

    Some rework

--- coroutines-jmh-benchmark/src/jmh/kotlin/com/example/coroutines/CreateDetachedTaskPayloadJmh.kt
+++ coroutines-jmh-benchmark/src/jmh/kotlin/com/example/coroutines/CreateDetachedTaskPayloadJmh.kt
@@ -1,12 +1,7 @@
 package com.example.coroutines
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import org.openjdk.jmh.annotations.*
 import java.util.*
 import java.util.concurrent.CompletableFuture
@@ -17,17 +12,13 @@ import java.util.stream.Collectors
 @State(Scope.Benchmark)
 open class CreateDetachedTaskPayloadJmh {
 
-    @Setup
-    fun setUp() {
-        corCounter.set(0)
-    }
+//    @Setup
+//    fun setUp() {
+//    }
 
-    @TearDown
-    fun end() {
-        print("Count: ${corCounter.get()} ")
-    }
-
-    private val corCounter = AtomicLong(0)
+//    @TearDown
+//    fun end() {
+//    }
 
     private val tasks = List(500) {
         MockDetachedTask(
@@ -42,9 +33,7 @@ open class CreateDetachedTaskPayloadJmh {
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_A_payload_simple() {
-        corCounter.set(0)
         tasks.map {
-            corCounter.incrementAndGet()
             DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                 task = it,
                 topic = "simpleTopic",
@@ -58,9 +47,7 @@ open class CreateDetachedTaskPayloadJmh {
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_B_payload_using_parallel_stream() {
-        corCounter.set(0)
         tasks.parallelStream().map {
-            corCounter.incrementAndGet()
             DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                 task = it,
                 topic = "parallelStreamTopic",
@@ -74,10 +61,8 @@ open class CreateDetachedTaskPayloadJmh {
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_C_payload_completable_future() {
-        corCounter.set(0)
         val futures = tasks.map {
             CompletableFuture.supplyAsync {
-                corCounter.incrementAndGet()
                 DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                     task = it,
                     topic = "simpleTopic",
@@ -94,10 +79,8 @@ open class CreateDetachedTaskPayloadJmh {
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_D_payload_using_coroutines_default() = runBlocking {
-        corCounter.set(0)
         tasks.map {
             async {
-                corCounter.incrementAndGet()
                 DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                     task = it,
                     topic = "coroutineTopic",
@@ -112,9 +95,7 @@ open class CreateDetachedTaskPayloadJmh {
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_E_payload_using_coroutines_parallel() {
-        corCounter.set(0)
         tasks.forEachParallel {
-            corCounter.incrementAndGet()
             DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                 task = it,
                 topic = "coroutineTopic",
@@ -125,12 +106,12 @@ open class CreateDetachedTaskPayloadJmh {
         }
     }
 
-    @Benchmark
-    @Throws(InterruptedException::class)
-    open fun mode_F_A_payload_using_windowed_tasks_coroutines_100(
-    ) {
-       runParametrized(100)
-    }
+//    @Benchmark
+//    @Throws(InterruptedException::class)
+//    open fun mode_F_A_payload_using_windowed_tasks_coroutines_100(
+//    ) {
+//       runParametrized(100)
+//    }
 
     @Benchmark
     @Throws(InterruptedException::class)
@@ -139,19 +120,17 @@ open class CreateDetachedTaskPayloadJmh {
        runParametrized(50)
     }
 
-    @Benchmark
-    @Throws(InterruptedException::class)
-    open fun mode_F_C_payload_using_windowed_tasks_coroutines_25(
-    ) {
-       runParametrized(25)
-    }
+//    @Benchmark
+//    @Throws(InterruptedException::class)
+//    open fun mode_F_C_payload_using_windowed_tasks_coroutines_25(
+//    ) {
+//       runParametrized(25)
+//    }
 
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_G_payload_using_buffered_flow() = runBlocking {
-        corCounter.set(0)
         tasks.asFlow().buffer(100).collect {
-            corCounter.incrementAndGet()
             DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                 task = it,
                 topic = "coroutineTopic",
@@ -159,35 +138,61 @@ open class CreateDetachedTaskPayloadJmh {
                 offset = 1,
                 taskHolderId = 123
             )
+        }
+    }
+
+    @Benchmark
+    @Throws(InterruptedException::class)
+    open fun mode_Z_payload_using_correct_buffered_flow() = runBlocking {
+        coroutineScope {
+            tasks.asFlow().map {
+                async {
+                    DetachedTaskPayloadFactory.createPayloadByDetachedTask(
+                        task = it,
+                        topic = "coroutineTopic",
+                        partition = 1,
+                        offset = 1,
+                        taskHolderId = 123
+                    )
+                }
+            }.buffer(100).map { it.await() }.collect {
+                val s = it.partition
+                s + s
+            }
         }
     }
 
     @Benchmark
     @Throws(InterruptedException::class)
     open fun mode_H_payload_using_buffered_flow_2() = runBlocking {
-        corCounter.set(0)
+
         val flow = flow {
             tasks.map { emit(it) }
+        }.flowOn(Dispatchers.IO)
+
+        val scope = CoroutineScope(Dispatchers.Default)
+
+        val job = scope.launch {
+            flow.map {
+                DetachedTaskPayloadFactory.createPayloadByDetachedTask(
+                    task = it,
+                    topic = "coroutineTopic",
+                    partition = 1,
+                    offset = 1,
+                    taskHolderId = 123
+                )
+            }.buffer(100).collect {
+                it.partition + it.offset
+            }
         }
 
-        flow.buffer(100).collect {
-            corCounter.incrementAndGet()
-            DetachedTaskPayloadFactory.createPayloadByDetachedTask(
-                task = it,
-                topic = "coroutineTopic",
-                partition = 1,
-                offset = 1,
-                taskHolderId = 123
-            )
-        }
+        job.join()
     }
 
     private fun runParametrized(windowSize: Int) = runBlocking {
-        corCounter.set(0)
         tasks.windowed(windowSize, windowSize, true).map { window ->
             async {
                 window.map {
-                    corCounter.incrementAndGet()
                     DetachedTaskPayloadFactory.createPayloadByDetachedTask(
                         task = it,
                         topic = "coroutineTopic",
